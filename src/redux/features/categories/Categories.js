a4d1081a24ffb92f6a862ff31f7e0dd307bea9ba
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-05 18:45:38 -0500

    Fix correction.  Add checkStatus as a reducer in categoriesSlice.js

--- src/redux/features/categories/Categories.js
+++ src/redux/features/categories/Categories.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from './categoriesSlice';
 
 const Categories = () => {
   const categories = useSelector((state) => state.categories.categories);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkStatus());
+  }, [dispatch]);
 
   return (
     <section>
--- src/redux/features/categories/categoriesSlice.js
+++ src/redux/features/categories/categoriesSlice.js
@@ -1,13 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  categories: ['Under Construction'],
+  categories: [],
 };
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
-  reducers: {},
+  reducers: {
+    checkStatus: (state) => {
+      state.categories.push('Under construction...');
+    },
+  },
 });
 
+export const { checkStatus } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
