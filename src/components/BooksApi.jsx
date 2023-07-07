7fe153a7923ac96c77d96b0495337a26fc544ee3
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-07 18:07:32 -0500

    Create new booksApiSlice.js to refactor previous Slice to fetch Bookstore API

---
+++ src/components/BooksApi.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBooks, deleteBook, getBooks } from '../redux/features/books/booksApiSlice';
+
+const Books = () => {
+  const books = useSelector((state) => state.books.books);
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleAddBook = () => {
+    const newBook = {
+      itemId: uuidv4(),
+      title,
+      author,
+      category,
+    };
+    dispatch(addBooks(newBook));
+    setTitle('');
+    setAuthor('');
+    setCategory('');
+  };
+
+  const handleDeleteBook = (itemId) => {
+    dispatch(deleteBook(itemId));
+  };
+
+  return (
+    <section className="books-container">
+      <input
+        type="text"
+        value={title}
+        placeholder="Add book title..."
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        value={author}
+        placeholder="Add book author..."
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <input
+        type="text"
+        value={category}
+        placeholder="Add book category..."
+        onChange={(e) => setCategory(e.target.value)}
+      />
+      <button type="submit" onClick={handleAddBook}>
+        ADD NEW BOOK
+      </button>
+      <p className="number-books">
+        Number of Books:
+        (
+        {books.length}
+        )
+      </p>
+      <ul className="booklist">
+        {books.map((book) => (
+          <div key={book.itemId} className="book-item">
+            <li className="book-category">{book.category}</li>
+            <li className="book-title">{book.title}</li>
+            <li className="book-author">{book.author}</li>
+            <div className="book-buttons">
+              <button
+                type="button"
+                className="delete-button"
+                onClick={() => handleDeleteBook(book.itemId)}
+              >
+                Delete
+              </button>
+              <button type="button" className="comment-button">Comment</button>
+              <button type="button" className="edit-button">Edit</button>
+            </div>
+            <li className="book-status">
+              {book.status}
+              {' '}
+              Completed
+            </li>
+            <li className="current-chapter">CURRENT CHAPTER</li>
+            <li className="book-chapter">{book.current_chapter}</li>
+            <button type="button" className="update-button">UPDATE PROGRESS</button>
+          </div>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Books;
---
+++ src/redux/features/books/booksApiSlice.js
@@ -0,0 +1,92 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/qiKtD5mkwRN26fLwBUzY/books/';
+
+export const getBooks = createAsyncThunk('books/getBooks',
+  async (thunkAPI) => {
+    try {
+      const res = await axios(url);
+      const resp = res.data;
+      const array = Object.values(resp);
+      const id = Object.keys(resp);
+      const newArray = [];
+      array.forEach((ele, i) => {
+        newArray.push({
+          ...ele[0],
+          item_id: id[i],
+        });
+      });
+      return newArray;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  });
+
+export const addBooks = createAsyncThunk('books/addBooks',
+  async (obj, thunkAPI) => {
+    try {
+      const res = await axios.post(url, obj);
+      thunkAPI.dispatch(getBooks());
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  });
+
+export const deleteBook = createAsyncThunk('books/deleteBook',
+  async (id, thunkAPI) => {
+    try {
+      const res = await axios.delete(`${url}/${id}`);
+      thunkAPI.dispatch(getBooks());
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  });
+
+const initialState = {
+  books: [],
+  isLoading: false,
+  error: undefined,
+  addNew: false,
+  deleted: false,
+};
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBooks.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getBooks.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.books = payload;
+      })
+      .addCase(getBooks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.name;
+      })
+      .addCase(addBooks.pending, (state) => {
+        state.addNew = undefined;
+      })
+      .addCase(addBooks.fulfilled, (state) => {
+        state.addNew = true;
+      })
+      .addCase(addBooks.rejected, (state) => {
+        state.addNew = false;
+      })
+      .addCase(deleteBook.pending, (state) => {
+        state.deleted = false;
+      })
+      .addCase(deleteBook.fulfilled, (state) => {
+        state.deleted = true;
+      })
+      .addCase(deleteBook.rejected, (state) => {
+        state.deleted = false;
+      });
+  },
+});
+export default bookSlice.reducer;
